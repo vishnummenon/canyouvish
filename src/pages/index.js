7386557049256db9dd96f473a95944c6e4ea34f5
Vishnu <vishnu.m@keyvalue.systems>
2021-01-02 12:16:19 +0530

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/about/">About Me</Link>
+      <Header headerText="Hello World!" />
+    </div>
+  )
 }
